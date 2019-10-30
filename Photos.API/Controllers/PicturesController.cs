955cce8ed0792414d8e7e149152d7c733fa5f83f
Michael McDowell <michael.mcdowell@hotmail.co.uk>
2019-10-30 20:00:19 +0000

    Add project files.

---
+++ Photos.API/Controllers/PicturesController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Picture.api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PicturesController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<PicturesController> _logger;
+
+        public PicturesController(ILogger<PicturesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Picture> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new Picture
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
