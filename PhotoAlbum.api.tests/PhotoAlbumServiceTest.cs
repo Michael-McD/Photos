c12933ef32728180368888c34ffad2240d765000
Michael McDowell <michael.mcdowell@hotmail.co.uk>
2019-11-20 17:43:27 +0000

    Project renaming (casing only).

--- PhotoAlbum.api.tests/PhotoAlbumServiceTest.cs
+++ PhotoAlbum.api.tests/PhotoAlbumServiceTest.cs
@@ -6,7 +6,7 @@ using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
-using System.Web.Http;
+using System.Web;
 using Xunit;
 using FluentAssertions;
 using System.Linq;
